a91bc696df6ce605a12b04f8046674f5aa4f4a85
JustinRuiz <85268427+JustinRuiz@users.noreply.github.com>
2023-04-13 23:05:06 -0400

    Update main.py

--- Troll Bot 1/main.py
+++ Troll Bot 1/main.py
@@ -10,51 +10,38 @@ turtle.shape(image)
 
 still_asking = True
 
+r = turtle.Turtle()
+r.hideturtle()
+r.penup()
+r.goto(0, -234.0)
+
 while still_asking:
 
     user_input = screen.textinput(title= "User Entry", prompt= "What do you ant to know. Math, Reading, Geography")
+    
+    r.clear()
 
     if user_input.title() == 'Math':
         
         math_reply = screen.textinput(title= "Math Question", prompt= "What do you want to know about?")
 
-        r = turtle.Turtle()
-        r.hideturtle()
-        r.penup()
-        r.goto(0, -234.0)
         r.write(troll_response.math_logic[math_reply.title()])
-        r.clear()
 
     elif user_input.title() == "Reading":
 
         reading_reply = screen.textinput(title= "Reading Question", prompt= "What do you want to know about?")
 
-        r = turtle.Turtle()
-        r.hideturtle()
-        r.penup()
-        r.goto(0, -234.0)
         r.write(troll_response.reading_logic[reading_reply.title()])
-        r.clear()
 
     elif user_input.title() == "Geography":
 
         geo_reply = screen.textinput(title= "Geography Question", prompt= "What do you want to know about?")
 
-        r = turtle.Turtle()
-        r.hideturtle()
-        r.penup()
-        r.goto(0, -234.0)
         r.write(troll_response.geography_logic[geo_reply.title()])
-        r.clear()
 
     else:
 
-        r = turtle.Turtle()
-        r.hideturtle()
-        r.penup()
-        r.goto(0, -234.0)
         r.write("That's a invalid response")
-        r.clear()
         
 
     if user_input.title() == 'Exit':
